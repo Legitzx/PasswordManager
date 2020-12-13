20768e5d63aa3274f46ac769a3eb3b34fa485e22
lk <luciano.kho18@gmail.com>
2020-12-12 21:43:19 -0800

    Implemented Bcrypt

--- src/main/java/org/legitzxdevelopment/PasswordManager.java
+++ src/main/java/org/legitzxdevelopment/PasswordManager.java
@@ -1,11 +1,22 @@
 package org.legitzxdevelopment;
 
+import org.legitzxdevelopment.hashing.BCrypt;
+
 /**
  * Contributor(s): Luciano K
  * Description:
  */
 public class PasswordManager {
     public static void main(String[] args) {
-        System.out.println("TEST");
+        String originalPassword = "Thet0234)A0234A)$#@a";
+        String salt = BCrypt.gensalt(16);
+        String generatedSecuredPasswordHash = BCrypt.hashpw(originalPassword, salt);
+
+        System.out.println(generatedSecuredPasswordHash);
+
+        System.out.println(BCrypt.checkpw(originalPassword, generatedSecuredPasswordHash));
+
+        String s = BCrypt.hashpw(originalPassword, generatedSecuredPasswordHash);
+        System.out.println(s);
     }
 }
